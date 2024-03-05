38bacb424a7e03d9f6fd74039626a16b411aa001
Angelina <angelina.wischounig@gmail.com>
2024-03-05 19:17:47 +0100

    Button fully implemented + connection to the internet in AndoidManifest.xml

--- app/src/main/java/at/klu/se2einzelaufgabe/MainActivity.java
+++ app/src/main/java/at/klu/se2einzelaufgabe/MainActivity.java
@@ -11,8 +11,18 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class MainActivity extends AppCompatActivity {
 
+    private ExecutorService executor = Executors.newSingleThreadExecutor();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,5 +40,27 @@ public class MainActivity extends AppCompatActivity {
         String matr = field.getText().toString();
         TextView output = findViewById(R.id.txtOutput);
 
+        executor.submit(() -> {
+            try {
+
+                Socket socket = new Socket("se2-submission.aau.at", 20080);
+                BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+                bw.write(matr); // send the matrikelnummer to the server
+                bw.write("\n"); // important, otherwise the server will not recognize the end of the input!
+                bw.flush();
+                BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                String result = br.readLine();
+                //bw.close();
+                //br.close()
+                socket.close();
+                runOnUiThread(() -> {
+                    output.setText(result);
+                });
+
+
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
     }
 }
